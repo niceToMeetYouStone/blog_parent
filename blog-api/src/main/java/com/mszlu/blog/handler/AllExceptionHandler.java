cbe69588c77239a443a222f795f965d85033152e
石头 <2398391057@qq.com>
2022-08-03 15:38:04 +0800

    添加ResponseBody返回数据的结果

--- blog-api/src/main/java/com/mszlu/blog/handler/AllExceptionHandler.java
+++ blog-api/src/main/java/com/mszlu/blog/handler/AllExceptionHandler.java
@@ -4,6 +4,8 @@ package com.mszlu.blog.handler;
 import com.mszlu.blog.vo.Result;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 // 对加了aontroller的注解的进行拦截处理
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class AllExceptionHandler {
     // 进行异常处理，处理excaption的异常
     @ExceptionHandler(Exception.class)
+    @ResponseBody
     public Result doExcaption(Exception e) {
         e.printStackTrace();
         return Result.fail(-999, "系统异常");
